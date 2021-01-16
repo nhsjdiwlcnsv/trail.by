c28390999b18df0ff39f6e3c8aab76fe9eab0151
michaelshkaubski <michaelshkarubski@gmail.com>
2021-01-16 15:07:36 +0300

    Small design bugs fix

--- src/components/navigation/SwitchThemeButton.js
+++ src/components/navigation/SwitchThemeButton.js
@@ -3,9 +3,9 @@ import { HiOutlineMoon } from 'react-icons/hi'
 import styles from './SwitchThemeButton.module.css'
 
 export default function SwitchThemeButton({ onClick }) {
-  return (
-    <button className={styles.button} onClick={onClick}>
-      <HiOutlineMoon size="3em" />
-    </button>
-  )
+    return (
+        <button className={styles.button} onClick={onClick}>
+            <HiOutlineMoon size="3em" />
+        </button>
+    )
 }
